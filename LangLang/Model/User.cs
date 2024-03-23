3efd8665f7f0feb0b25cb369e543cd88985fcc05
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-23 17:00:18 +0100

    Add User class

---
+++ LangLang/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class User
+{
+	private string email;
+	private string password;
+	private string name;
+	private string surname;
+	private DateTime birthDate;
+	private string gender;
+	private string phoneNumber;
+
+
+
+	public User(string email, string password, string name, string surname, DateTime birthDate, string gender, string phoneNumber)
+	{
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.surname = surname; 
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.phoneNumber = phoneNumber;
+    }
+}
