3f7842af1777e188470fd2c82cef62a5583c91be
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-23 22:58:54 +0100

    Add StudentDAO class

---
+++ LangLang/DAO/StudentDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Consts;
+using System.Collections.Generic;
+
+public class StudentDAO
+{
+    private static StudentDAO instance;
+    Dictionary<string, Student> students;
+
+
+	private StudentDAO()
+	{
+	}
+
+    public static StudentDAO GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new StudentDAO();
+        }
+        return instance;
+    }
+
+    public Dictionary<string, Student> GetAllStudents()
+	{
+        if (students == null)
+        {
+            students = JsonUtil.loadAllStudents(Constants.StudentFilePath);
+        }
+		return students;
+    }
+
+	public Student FindStudent(string email)
+	{
+		return students[email];
+	}
+
+}
