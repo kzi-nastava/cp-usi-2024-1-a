3f829502011987b41fe00d5accd93b418c034cc0
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-24 14:37:13 +0100

    Add StudentService

---
+++ LangLang/Services/StudentService.cs
@@ -0,0 +1,45 @@
+﻿using Consts;
+using System;
+using System.Collections.Generic;
+
+public class StudentService
+{
+	StudentDAO studentDAO = StudentDAO.GetInstance();
+    public Student LoggedUser { get; set; }
+
+
+
+    //Singleton
+    private static StudentService instance;
+    private StudentService()
+    {
+    }
+
+    public static StudentService GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new StudentService();
+        }
+        return instance;
+    }
+
+    public void UpdateStudentData(string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber)
+    {
+        LoginService loginService = LoginService.GetInstance();
+        bool checkData = loginService.checkUserData(LoggedUser.Email, password, name, surname, phoneNumber);
+
+        if (checkData)
+        {
+            LoggedUser.Name = name;
+            LoggedUser.Surname = surname;
+            LoggedUser.BirthDate = birthDate;
+            LoggedUser.Gender = gender;
+            LoggedUser.PhoneNumber = phoneNumber;
+
+            studentDAO.AddStudent(LoggedUser);  //since its a hashmap it will replace it
+        }
+       
+    }
+
+}
