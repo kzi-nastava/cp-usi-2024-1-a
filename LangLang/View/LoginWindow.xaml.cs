a06ad43f7373fb715c46d067a0408f04cef44e8f
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-25 00:42:57 +0100

    Add basic Login

---
+++ LangLang/View/LoginWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace LangLang.View
+{
+    public partial class LoginWindow : Window
+    {
+        private LoginService loginService;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            loginService = LoginService.GetInstance();
+        }
+
+
+
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+            MessageBox.Show($"Email inputed: {email}\nPassword: {password}");
+
+
+
+            loginService.LogIn(email, password);
+
+
+            //LoginFailed
+            if (!loginService.validUser)
+            {
+                if (!loginService.validEmail)
+                {
+                    MessageBox.Show($"User doesn't exist", "", MessageBoxButton.OK);
+                }
+                else
+                {
+                    MessageBox.Show($"Wrong password", "", MessageBoxButton.OK);
+                }
+            }
+            else
+            {
+                StudentService ss = StudentService.GetInstance();
+                MessageBox.Show($"Successful login Email: {email}\nPassword: {password}\n Name{ss.LoggedUser.Name}");
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Placeholder for handling text change event
+        }
+    }
+}
+
---
+++ LangLang/ViewModel/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LangLang.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _email;
+        private string _password;
+        private readonly LoginService _loginService;
+
+        public LoginViewModel()
+        {
+            _loginService = LoginService.GetInstance();
+            LoginCommand = new RelayModel(Login);
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        private void Login(object parameter)
+        {
+            MessageBox.Show($"Email inputted: {Email}\nPassword: {Password}");
+
+            _loginService.LogIn(Email, Password);
+
+            if (!_loginService.validUser)
+            {
+                if (!_loginService.validEmail)
+                {
+                    MessageBox.Show("User doesn't exist", "", MessageBoxButton.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Wrong password", "", MessageBoxButton.OK);
+                }
+            }
+            else
+            {
+                StudentService ss = StudentService.GetInstance();
+                MessageBox.Show($"Successful login Email: {Email}\nPassword: {Password}\nName: {ss.LoggedUser.Name}");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
