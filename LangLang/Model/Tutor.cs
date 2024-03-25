2115e2905745680000da340c2426b1317b3c4140
Filip Tot <filipsombor@gmail.com>
2024-03-25 19:14:28 +0100

    Update Tutor to be in namespace

--- LangLang/Model/Tutor.cs
+++ LangLang/Model/Tutor.cs
@@ -2,21 +2,23 @@
 using System;
 using System.Collections.Generic;
 
-public class Tutor : User
+namespace LangLang.Model
 {
-    public List<Language> KnownLanguages {get; set;}
-    public List<Course> Courses { get; set; }
-    public double ScoreAvg { get; set; }
-    // TODO: create exam model and add it
-    //public List<Exam> Exams;
-    
-    
-    public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Language> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+    public class Tutor : User
     {
-        ScoreAvg = scoreAvg;
-        KnownLanguages = knownLanguages;
-        Courses = courses;
+        public List<Language> KnownLanguages { get; set; }
+        public List<Course> Courses { get; set; }
+        public double ScoreAvg { get; set; }
+        // TODO: create exam model and add it
+        //public List<Exam> Exams;
 
+
+        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Language> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        {
+            ScoreAvg = scoreAvg;
+            KnownLanguages = knownLanguages;
+            Courses = courses;
+
+        }
     }
-
 }
