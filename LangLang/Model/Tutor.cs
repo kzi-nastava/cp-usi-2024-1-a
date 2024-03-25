bef19001262d9c99dd0d02199ddce2380792f4a5
Milan Lazarevic <mlazarevic662@gmail.com>
2024-03-25 13:05:54 +0100

    Delete unused attributes

--- LangLang/Model/Tutor.cs
+++ LangLang/Model/Tutor.cs
@@ -4,15 +4,11 @@ using System.Collections.Generic;
 
 public class Tutor : User
 {
-    private List<Language> knownLanguages;
-    private List<Course> courses;
-    private double scoreAvg;
-    // TODO: create exam model and add get/set and attribute in constructor
-    //public List<Exam> exams;
-
     public List<Language> KnownLanguages {get; set;}
     public List<Course> Courses { get; set; }
     public double ScoreAvg { get; set; }
+    // TODO: create exam model and add it
+    //public List<Exam> Exams;
     
     
     public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Language> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
