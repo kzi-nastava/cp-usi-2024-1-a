cd8500ed0226b596bb48f515fab212eb752dbf1f
Filip Tot <filipsombor@gmail.com>
2024-03-25 19:30:00 +0100

    Update Tutor KnownLanguages to include lvl

--- LangLang/Model/Tutor.cs
+++ LangLang/Model/Tutor.cs
@@ -6,19 +6,18 @@ namespace LangLang.Model
 {
     public class Tutor : User
     {
-        public List<Language> KnownLanguages { get; set; }
+        public List<Tuple<Language, LanguageLvl>> KnownLanguages { get; set; }
         public List<Course> Courses { get; set; }
         public double ScoreAvg { get; set; }
         // TODO: create exam model and add it
         //public List<Exam> Exams;
 
 
-        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Language> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Tuple<Language, LanguageLvl>> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
         {
             ScoreAvg = scoreAvg;
             KnownLanguages = knownLanguages;
             Courses = courses;
-
         }
     }
 }
