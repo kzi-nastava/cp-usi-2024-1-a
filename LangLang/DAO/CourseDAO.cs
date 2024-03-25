77c155bdeae1d04d3b971def1a28c9479a9e367d
Milan Lazarevic <mlazarevic662@gmail.com>
2024-03-25 15:16:37 +0100

    Add course DAO

---
+++ LangLang/DAO/CourseDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Consts;
+using System.Collections.Generic;
+
+namespace LangLang.DAO
+{
+    internal class CourseDAO
+    {
+        private static CourseDAO instance;
+        private Dictionary<string, Course> courses;
+
+        private CourseDAO()
+        {
+
+        }
+        public static CourseDAO getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new CourseDAO();
+            }
+            return instance;
+        }
+
+        public Dictionary<string, Course> getAllCourses()
+        {
+            if(courses == null)
+            {
+                courses = JsonUtil.ReadFromFile<Course>(Constants.CourseFilePath);
+            }
+            return courses;
+        }
+
+        public void AddCourse(Course course)
+        {
+            courses[course.Name] = course;
+            JsonUtil.WriteToFile<Course>(courses, Constants.CourseFilePath);
+        }
+
+        public void DeleteStudent(string name)
+        {
+            courses.Remove(name);
+        }
+
+
+    }
+}
