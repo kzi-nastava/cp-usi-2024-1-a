7820e03d89a87ae29aed83be64add7578f41183f
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-26 17:15:52 +0100

    Add missing using namespaces

--- LangLang/Model/Student.cs
+++ LangLang/Model/Student.cs
@@ -4,64 +4,65 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Xml.Linq;
 
-
-public class Student: User
+namespace LangLang.Model
 {
-    //---------------------------Languages
-    private List<string> coursesApplied;    //string is courseId
-    private List<string> notifications;
-
-
-    //--------------------------Properties
-    public string Qualification { get; set; }
-    public uint PenaltyPts { get; set; }
-    public string AttendingCourse { get; set; }
-
-    public Student() : base("", "", "", "", DateTime.Now, Gender.Other, "")
+    public class Student : User
     {
-        PenaltyPts = 0;
-        AttendingCourse = "";
-        Qualification = "";
-        this.coursesApplied = new List<string>();
-        this.notifications = new List<string>();
-    }
-
-    public Student(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, string qualification, uint penaltyPts, string attendingCourse, List<string> coursesApplied, List<string> notifications)
-        : base(email, password, name, surname, birthDate, gender, phoneNumber)
-    {
-        PenaltyPts = penaltyPts;
-        AttendingCourse = attendingCourse;
-        Qualification = qualification;
-        this.coursesApplied = coursesApplied;
-        this.notifications = notifications;
-	}
-
-    public void RemovePenaltyPts()
-    {
-        if (PenaltyPts > 0) { PenaltyPts--; }
-    }
+        //---------------------------Languages
+        private List<string> coursesApplied;    //string is courseId
+        private List<string> notifications;
 
 
+        //--------------------------Properties
+        public string Qualification { get; set; }
+        public uint PenaltyPts { get; set; }
+        public string AttendingCourse { get; set; }
 
-    public void AddCourse(string course)
-    {
-        coursesApplied.Add(course);
-    }
+        public Student() : base("", "", "", "", DateTime.Now, Gender.Other, "")
+        {
+            PenaltyPts = 0;
+            AttendingCourse = "";
+            Qualification = "";
+            this.coursesApplied = new List<string>();
+            this.notifications = new List<string>();
+        }
 
-    public List<string> GetAppliedCourses()
-    {
-        return coursesApplied;
-    }
+        public Student(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, string qualification, uint penaltyPts, string attendingCourse, List<string> coursesApplied, List<string> notifications)
+            : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        {
+            PenaltyPts = penaltyPts;
+            AttendingCourse = attendingCourse;
+            Qualification = qualification;
+            this.coursesApplied = coursesApplied;
+            this.notifications = notifications;
+        }
 
-    public void AddNotification(string notification)
-    {
-        notifications.Add(notification);
-    }
+        public void RemovePenaltyPts()
+        {
+            if (PenaltyPts > 0) { PenaltyPts--; }
+        }
 
-    public List<string> GetNotifications()
-    {
-        return notifications;
+
+
+        public void AddCourse(string course)
+        {
+            coursesApplied.Add(course);
+        }
+
+        public List<string> GetAppliedCourses()
+        {
+            return coursesApplied;
+        }
+
+        public void AddNotification(string notification)
+        {
+            notifications.Add(notification);
+        }
+
+        public List<string> GetNotifications()
+        {
+            return notifications;
+        }
     }
 }
-
 
