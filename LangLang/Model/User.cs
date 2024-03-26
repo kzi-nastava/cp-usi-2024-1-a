ffdc8dc3d15e760fe874b4e1dc28c9d07da15977
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-26 18:01:06 +0100

    Add namespace for Language and User

--- LangLang/Model/User.cs
+++ LangLang/Model/User.cs
@@ -2,24 +2,27 @@
 using System;
 using System.Collections.Generic;
 
-public abstract class User
+namespace LangLang.Model
 {
-	public string Email { get; set; }
-	public string Password { get; set; }	
-	public string Name { get; set; }
-    public string Surname { get; set; }
-    public DateTime BirthDate { get; set; }
-	public Gender Gender { get; set; }
-	public string PhoneNumber { get; set; }
+    public abstract class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string PhoneNumber { get; set; }
 
-    public User(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber)
-	{
-		Email = email;
-		Password = password;
-		Name = name;
-		Surname = surname; 
-		BirthDate = birthDate;
-		Gender = gender;
-		PhoneNumber = phoneNumber;
+        public User(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber)
+        {
+            Email = email;
+            Password = password;
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+            Gender = gender;
+            PhoneNumber = phoneNumber;
+        }
     }
 }
