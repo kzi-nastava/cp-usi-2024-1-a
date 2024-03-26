e35514a3310fe86a92370670a7e8677d98249af4
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-26 17:12:53 +0100

    Add missing namespace and include in appropriate files

--- LangLang/Services/StudentService.cs
+++ LangLang/Services/StudentService.cs
@@ -1,6 +1,8 @@
 ﻿using Consts;
 using System;
 using System.Collections.Generic;
+using System.Windows;
+using LangLang.Model;
 
 public class StudentService
 {
@@ -26,9 +28,14 @@ public class StudentService
 
     public void UpdateStudentData(string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber)
     {
-        LoginService loginService = LoginService.GetInstance();
-        bool checkData = loginService.checkUserData(LoggedUser.Email, password, name, surname, phoneNumber);
+        if (LoggedUser.AttendingCourse != "")
+        {
+            MessageBox.Show($"User is attending a course!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
 
+        LoginService loginService = LoginService.GetInstance();
+        bool checkData = loginService.CheckUserData(LoggedUser.Email, password, name, surname, phoneNumber);
         if (checkData)
         {
             LoggedUser.Name = name;
@@ -40,6 +47,29 @@ public class StudentService
             studentDAO.AddStudent(LoggedUser);  //since its a hashmap it will replace it
         }
        
+    }
+
+    /*
+    public void DeleteMyAccount()
+    {
+        CourseService cs = CourseService.getInstance();
+        for (string courseID: LoggedUser.GetAppliedCourses)
+        {
+            cs.removeStudent(LoggedUser.Email, courseID);
+        }
+
+        cs.removeApplied(LoggedUser.Email, LoggedUser.AttendingCourse);
+
+
+        studentDAO.DeleteStudent(LoggedUser.Email);
+    }
+    */
+
+    public void ApplyForCourse(string courseId)
+    {
+        LoggedUser.AddCourse(courseId);
+        //Alert courses to add me
+        //courseService.addStudent(LoggedUser.Email)
     }
 
 }
