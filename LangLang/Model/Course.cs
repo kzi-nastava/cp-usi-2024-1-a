9333f056aa5caddc2c443a58e3ecbba63a6978ba
Milan Lazarevic <mlazarevic662@gmail.com>
2024-03-27 14:24:00 +0100

    Update enum constants to pascal case, remove empty constructor, Add defaultvalue in constructor

--- LangLang/Model/Course.cs
+++ LangLang/Model/Course.cs
@@ -10,7 +10,7 @@ namespace LangLang.Model
         public Language Language { get; set; }
         public LanguageLvl Level { get; set; }
         public int Duration { get; set; }
-        public List<WorkDay> Schedule { get; set; }
+        public Dictionary<WorkDay,Tuple<TimeOnly,int>> Schedule { get; set; }
         public DateTime Start { get; set; }
         public bool Online { get; set; }
         public int MaxStudents { get; set; }
@@ -23,28 +23,9 @@ namespace LangLang.Model
         public double ListeningAvgScore { get; set; }
         public double SpeakingAvgScore { get; set; }
 
-        public Course()
-        {
-            Name = "";
-            Language = new Language("English", "en");
-            Level = LanguageLvl.A1;
-            Duration = 0;
-            Schedule = new List<WorkDay> { WorkDay.MON };
-            Start = DateTime.Now;
-            Online = false;
-            MaxStudents = 0;
-            NumStudents = 0;
-            State = CourseState.ACTIVE;
-            //set default values for attributes for reports
-            NumPenaltyPts = 0;
-            NumStudentsPassed = 0;
-            ReadingAvgScore = 0;
-            WritingAvgScore = 0;
-            ListeningAvgScore = 0;
-            SpeakingAvgScore = 0;
-        }
+        
 
-        public Course(string name, Language language, LanguageLvl level, int duration, List<WorkDay> schedule, DateTime start, bool online, int maxStudents, int numStudents, CourseState state)
+        public Course(string name, Language language, LanguageLvl level, int duration, Dictionary<WorkDay, Tuple<TimeOnly, int>> schedule, DateTime start, bool online, int numStudents, CourseState state, int maxStudents = 0)
         {
             Name = name;
             Language = language;
@@ -64,29 +45,7 @@ namespace LangLang.Model
             ListeningAvgScore = 0;
             SpeakingAvgScore = 0;
         }
-        // Without maxStudents parameter, for online courses
-        public Course(string name, Language language, LanguageLvl level, int duration, List<WorkDay> schedule, DateTime start, bool online, CourseState state, int numStudents)
-        {
-            Name = name;
-            Language = language;
-            Level = level;
-            Duration = duration;
-            Schedule = schedule;
-            Start = start;
-            Online = online;
-            MaxStudents = int.MaxValue;
-            NumStudents = numStudents;
-            State = state;
-            //set default values for attributes for reports
-            NumPenaltyPts = 0;
-            NumStudentsPassed = 0;
-            ReadingAvgScore = 0;
-            WritingAvgScore = 0;
-            ListeningAvgScore = 0;
-            SpeakingAvgScore = 0;
-
-        }
-
+        
 
         public void AddAttendance()
         {
