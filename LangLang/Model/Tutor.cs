22e6116df33d110552be307bf84470aeb5777ffa
Filip Tot <filipsombor@gmail.com>
2024-03-27 22:11:30 +0100

    Update Tutor to count seperate scores

--- LangLang/Model/Tutor.cs
+++ LangLang/Model/Tutor.cs
@@ -1,23 +1,36 @@
 ﻿using Consts;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace LangLang.Model
 {
     public class Tutor : User
     {
+        /// <summary> Hold counts for 1 to 5 rating. </summary>
+        private int[] scoreCounts = new int[5];
+
         public List<Tuple<Language, LanguageLvl>> KnownLanguages { get; set; }
         public List<Course> Courses { get; set; }
         public List<Exam> Exams { get; set; }
-        public double ScoreAvg { get; set; }
 
-
-        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Tuple<Language, LanguageLvl>> knownLanguages, List<Course> courses, List<Exam> exams, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Tuple<Language, LanguageLvl>> knownLanguages, List<Course> courses, List<Exam> exams, int[] scoreCounts) : base(email, password, name, surname, birthDate, gender, phoneNumber)
         {
-            ScoreAvg = scoreAvg;
             KnownLanguages = knownLanguages;
             Courses = courses;
             Exams = exams;
+            this.scoreCounts = scoreCounts;
+        }
+
+        public double GetAverageScore()
+        {
+            int sum = 0;
+            for (int i = 0; i < scoreCounts.Length; i++)
+            {
+                sum += (i+1) * scoreCounts[i];
+            }
+            return (double)sum / scoreCounts.Length;
         }
     }
 }
