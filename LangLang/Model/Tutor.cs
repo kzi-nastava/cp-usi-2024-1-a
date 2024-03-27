1fd2c820d2bea07a22a05bb8613be4bad0c74293
Filip Tot <filipsombor@gmail.com>
2024-03-27 21:04:15 +0100

    Add exams list to Tutor class

--- LangLang/Model/Tutor.cs
+++ LangLang/Model/Tutor.cs
@@ -8,16 +8,16 @@ namespace LangLang.Model
     {
         public List<Tuple<Language, LanguageLvl>> KnownLanguages { get; set; }
         public List<Course> Courses { get; set; }
+        public List<Exam> Exams { get; set; }
         public double ScoreAvg { get; set; }
-        // TODO: create exam model and add it
-        //public List<Exam> Exams;
 
 
-        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Tuple<Language, LanguageLvl>> knownLanguages, List<Course> courses, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        public Tutor(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, List<Tuple<Language, LanguageLvl>> knownLanguages, List<Course> courses, List<Exam> exams, double scoreAvg) : base(email, password, name, surname, birthDate, gender, phoneNumber)
         {
             ScoreAvg = scoreAvg;
             KnownLanguages = knownLanguages;
             Courses = courses;
+            Exams = exams;
         }
     }
 }
