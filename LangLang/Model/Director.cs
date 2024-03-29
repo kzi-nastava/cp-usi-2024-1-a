d6f95ae52b30691abadd511e391866b0ff4eee35
FilipT03 <filipsombor@gmail.com>
2024-03-29 21:14:18 +0100

    Add Director default constructor

--- LangLang/Model/Director.cs
+++ LangLang/Model/Director.cs
@@ -9,6 +9,10 @@ namespace LangLang.Model
 {
     public class Director : User
     {
+        public Director() : base("", "", "", "", DateTime.Now, Gender.Other, "")
+        {
+        }
+
         public Director(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber) : base(email, password, name, surname, birthDate, gender, phoneNumber)
         {
         }
