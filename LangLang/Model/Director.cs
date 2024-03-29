293023d598742e811f8eced6a83b6b7b87f60f86
FilipT03 <filipsombor@gmail.com>
2024-03-29 21:00:23 +0100

    Add Director model

---
+++ LangLang/Model/Director.cs
@@ -0,0 +1,16 @@
+﻿using Consts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.Model
+{
+    public class Director : User
+    {
+        public Director(string email, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber) : base(email, password, name, surname, birthDate, gender, phoneNumber)
+        {
+        }
+    }
+}
