3017f54579790fb17e8d48923eb029c55bbd5920
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-03-29 22:46:35 +0100

    Move input validation to registrationService

--- LangLang/Services/LoginService.cs
+++ LangLang/Services/LoginService.cs
@@ -62,51 +62,7 @@ public class LoginService
         }
     }
 
-    public void RegisterStudent(string email, string password, string name, string surname, DateTime birthDay, Gender gender, string phoneNumber, string qualification)
-    {
-        StudentDAO sd = StudentDAO.GetInstance();
-        bool passed = CheckUserData(email, password, name, surname, phoneNumber);
-        passed = !(CheckExistingEmail(email)); 
-
-        if (passed)
-        {
-            sd.AddStudent(new Student(email, password, name, surname, birthDay, gender, phoneNumber, qualification, 0, "", null, null));
-        }
-    }
-
-    public bool CheckExistingEmail(string email)
-    {
-        StudentDAO sd = StudentDAO.GetInstance();
-        //other daos
-
-        if (sd.GetStudent(email) != null)  //or other searches
-        {
-            return true;
-        }
-        return false;
-    }
-
-    public bool CheckUserData(string email, string password, string name, string surname, string phoneNumber)
-    {
-        bool passed = true;
-        try
-        {
-            _ = new MailAddress(email);
-        }
-        catch
-        {
-            passed = false;
-        }
-
-        passed = !(int.TryParse(name, out _));      //checking if it's solely letters
-        passed = !(int.TryParse(surname, out _));
-        passed = int.TryParse(phoneNumber, out _);  //checking if it's solely numeric
-
-        passed = password.Length > 8;               //password must include numbers, an upper character and should be longer than 8
-        passed = !(password.Any(char.IsDigit));
-        passed = !(password.Any(char.IsUpper));
-        return passed;
-    }
+    
 
 }
 
