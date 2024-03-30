f4e891787f2f8fce6bfdd10358760599cb5e59d4
Milan Lazarevic <mlazarevic662@gmail.com>
2024-03-30 09:42:39 +0100

    Add Event listener for multi select combobox.

---
+++ LangLang/View/CourseWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LangLang.ViewModel;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace LangLang.View
+{
+    /// <summary>
+    /// Interaction logic for CourseWindow.xaml
+    /// </summary>
+    public partial class CourseWindow : Window
+    {
+        public CourseWindow()
+        {
+            InitializeComponent();
+            DataContext = new CourseViewModel(this);
+
+        }
+        private void cbSchedule_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var viewModel = DataContext as CourseViewModel;
+            if (viewModel != null)
+            {
+                var selectedItems = new ObservableCollection<Consts.WorkDay>();
+                foreach (var item in cbSchedule.SelectedItems)
+                {
+                    selectedItems.Add((Consts.WorkDay)item);
+                }
+                viewModel.ScheduleDays = selectedItems;
+            }
+        }
+    }
+}
