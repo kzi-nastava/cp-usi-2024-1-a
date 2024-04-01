4b09ae7f1ba8c45de862c655b5be66c392aa70a5
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-04-01 18:35:11 +0200

    Fix minor bug in student service

--- LangLang/Services/StudentService.cs
+++ LangLang/Services/StudentService.cs
@@ -7,10 +7,8 @@ using LangLang.Services;
 
 public class StudentService
 {
-	StudentDAO studentDAO = StudentDAO.GetInstance();
+    StudentDAO studentDAO = StudentDAO.GetInstance();
     public Student LoggedUser { get; set; }
-
-
 
     //Singleton
     private static StudentService instance;
@@ -29,9 +27,9 @@ public class StudentService
 
     public bool UpdateStudent(string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber)
     {
-        if (LoggedUser.AttendingCourse != "")
+        if (LoggedUser.AttendingCourse != "" || LoggedUser.AttendingExam != "" || LoggedUser.GetAppliedCourses().Count != 0 || LoggedUser.GetAppliedExams().Count != 0)
         {
-            MessageBox.Show($"User is attending a course!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //MessageBox.Show($"User is attending a course!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             return false;
         }
 
@@ -47,27 +45,34 @@ public class StudentService
         return true;
     }
 
-    /*
+    
     public void DeleteMyAccount()
     {
-        CourseService cs = CourseService.getInstance();
-        for (string courseID: LoggedUser.GetAppliedCourses)
+        CourseService cs = new();
+        foreach (string courseID in LoggedUser.GetAppliedCourses())
         {
-            cs.removeStudent(LoggedUser.Email, courseID);
+            Course course = cs.GetCourseById(courseID);
+            course.CancelAttendance();
+            cs.UpdateCourse(course);
         }
 
-        cs.removeApplied(LoggedUser.Email, LoggedUser.AttendingCourse);
+        foreach(string examID in LoggedUser.GetAppliedExams())
+        {
+            /**/
+        }
 
 
         studentDAO.DeleteStudent(LoggedUser.Email);
     }
-    */
+    
 
     public void ApplyForCourse(string courseId)
     {
         LoggedUser.AddCourse(courseId);
-        //Alert courses to add me
-        //courseService.addStudent(LoggedUser.Email)
+        CourseService cs = new();
+        Course course = cs.GetCourseById(courseId);
+        course.AddAttendance();
+        cs.UpdateCourse(course);
     }
 
 }
