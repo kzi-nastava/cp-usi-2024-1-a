b7e26a9a31c82ee53b0938fcc5d32c703bdaaf7b
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-04-28 20:43:14 +0200

    Update course/exam overview for student

--- LangLang/View/StudentWindow.xaml.cs
+++ LangLang/View/StudentWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using LangLang.Model;
-using LangLang.Services;
 using LangLang.ViewModel;
 using System.Windows;
 using LangLang.MVVM;
-using LangLang.Services.UserServices;
 using LangLang.View.Factories;
 using System.Windows.Controls;
 
@@ -18,68 +16,6 @@ namespace LangLang.View
             DataContext = studentViewModel;
         }
         
-        /*
-        private void CancelAtendingCourseButton(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is Course course)
-            {
-                if (DataContext is StudentViewModel viewModel)
-                {
-                    viewModel.CancelAttendingCourseCommand.Execute(course);
-                }
-            }
-        }
-        */
-
-
-
-        private void ApplyButtonCourse(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is Course course)
-            {
-                string courseId = course.Id;
-                if (DataContext is StudentViewModel viewModel)
-                {
-                    viewModel.ApplyCourseCommand.Execute(courseId);
-                }
-            }
-        }
-
-        private void CancelButtonCourse(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is Course course)
-            {
-                string courseId = course.Id;
-                if (DataContext is StudentViewModel viewModel)
-                {
-                    viewModel.CancelCourseCommand.Execute(courseId);
-                }
-            }
-        }
-
-        private void ApplyButtonExam(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is Exam exam)
-            {
-                string examId = exam.Id;
-                if (DataContext is StudentViewModel viewModel)
-                {
-                    viewModel.ApplyExamCommand.Execute(examId);
-                }
-            }
-        }
-
-        private void CancelButtonExam(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is Exam exam)
-            {
-                string examId = exam.Id;
-                if (DataContext is StudentViewModel viewModel)
-                {
-                    viewModel.CancelExamCommand.Execute(examId);
-                }
-            }
-        }
 
         private void RateTutorButton(object sender, RoutedEventArgs e)
         {
