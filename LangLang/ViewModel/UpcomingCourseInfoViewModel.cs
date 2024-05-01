2c09499c31f56dfffa1f32cde8fffc930438c726
Milan Lazarevic <mlazarevic662@gmail.com>
2024-05-01 09:50:00 +0200

    Add all GUI elements needed for upcomingCourseInfo

--- LangLang/ViewModel/UpcomingCourseInfoViewModel.cs
+++ LangLang/ViewModel/UpcomingCourseInfoViewModel.cs
@@ -1,26 +1,63 @@
-﻿using LangLang.Model;
+﻿using LangLang.DAO;
+using LangLang.Model;
 using LangLang.MVVM;
 using LangLang.Stores;
-using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace LangLang.ViewModel
 {
     public class UpcomingCourseInfoViewModel : ViewModelBase, INavigableDataContext
     {
         public NavigationStore NavigationStore { get; }
+
         private readonly CurrentCourseStore _currentCourseStore;
+
+        private readonly IStudentDAO _studentDAO;
         public RelayCommand AcceptStudentCommand { get; }
         public RelayCommand DenyStudentCommand { get; }
-        public RelayCommand GivePenaltyPointCommand { get; }
-        public RelayCommand GradeStudentCommand { get; }
 
         private string courseName = "";
+        private string name = "";
+        private string surname = "";
+        private string email = "";
+        private string message = "";
+        private uint penaltyPts;
+        public string Name
+        {
+            get => name;
+            set => SetField(ref name, value);
+        }
+        public string Surname
+        {
+            get => surname;
+            set => SetField(ref surname, value);
+        }
+        public string Email
+        {
+            get => email;
+            set => SetField(ref email, value);
+        }
+        public uint PenaltyPts
+        {
+            get => penaltyPts;
+            set => SetField(ref penaltyPts, value);
+        }
         public string CourseName
         {
             get => courseName;
             set
             {
                 SetField(ref courseName, value);
+            }
+        }
+        public string Message
+        {
+            get => message;
+            set
+            {
+                SetField(ref message, value);
             }
         }
 
@@ -31,39 +68,54 @@ namespace LangLang.ViewModel
             set
             {
                 SetField(ref selectedStudent, value);
+                SelectStudent();
             }
         }
-        //public ObservableCollection<Student> Students { get; set; }
 
-        public UpcomingCourseInfoViewModel(NavigationStore navigationStore, CurrentCourseStore currentCourseStore)
+        public ObservableCollection<Student> Students { get; set; }
+
+        public UpcomingCourseInfoViewModel(NavigationStore navigationStore, CurrentCourseStore currentCourseStore, IStudentDAO studentDAO)
         {
             NavigationStore = navigationStore;
             _currentCourseStore = currentCourseStore;
+            _studentDAO = studentDAO;
+            Students = new ObservableCollection<Student>(LoadStudents());
             CourseName = _currentCourseStore.CurrentCourse!.Name;
             AcceptStudentCommand = new RelayCommand(AcceptStudent, canExecute => SelectedStudent != null);
             DenyStudentCommand = new RelayCommand(DenyStudent, canExecute => SelectedStudent != null);
-            GivePenaltyPointCommand = new RelayCommand(GivePenaltyPoint, canExecute => SelectedStudent != null);
-            GradeStudentCommand = new RelayCommand(GradeStudent, canExecute => SelectedStudent != null);
         }
 
-        private void GradeStudent(object? obj)
+        private List<Student> LoadStudents()
         {
-            throw new NotImplementedException();
+            List<Student> students = new List<Student>();
+            foreach (Student student in _studentDAO.GetAllStudents().Values)
+            {
+                students.Add(student);
+            }
+            return students;
         }
-
-        private void GivePenaltyPoint(object? obj)
+        private void SelectStudent()
         {
-            throw new NotImplementedException();
+            if (SelectedStudent == null) return;
+            Name = SelectedStudent.Name;
+            Surname = SelectedStudent.Surname;
+            Email = SelectedStudent.Email;
+            PenaltyPts = SelectedStudent.PenaltyPts;
+
         }
 
         private void DenyStudent(object? obj)
         {
-            throw new NotImplementedException();
+            // TODO: call course coordinator to reject student
+            // TODO: refresh the list
+            MessageBox.Show("Student is denied!", "Success");
         }
 
         private void AcceptStudent(object? obj)
         {
-            throw new NotImplementedException();
+            // TODO: call course coordinator to transform course application to course attendandce
+            // TODO: change course number
+            MessageBox.Show("Student is accepted.", "Success");
         }
     }
 }
