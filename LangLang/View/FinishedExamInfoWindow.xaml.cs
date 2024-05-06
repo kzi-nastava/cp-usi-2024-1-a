124f766152ecc8547511ba02255b71bbecb11c9f
FilipT03 <filipsombor@gmail.com>
2024-05-06 04:24:27 +0200

    Add FinishedExamInfo window

---
+++ LangLang/View/FinishedExamInfoWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using LangLang.MVVM;
+using LangLang.View.Factories;
+using LangLang.ViewModel;
+
+namespace LangLang.View
+{
+    public partial class FinishedExamInfoWindow : NavigableWindow
+    {
+        public FinishedExamInfoWindow(FinishedExamInfoViewModel finishedExamInfoViewModel, ILangLangWindowFactory windowFactory)
+            : base(finishedExamInfoViewModel, windowFactory)
+        {
+            InitializeComponent();
+            DataContext = finishedExamInfoViewModel;
+        }
+    }
+}
