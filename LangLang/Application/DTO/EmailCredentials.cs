9d5aca0e8e76373dafffa6399ce42e061ee96a5a
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-05-17 16:28:25 +0200

    Add email credentials to appsettings.json

---
+++ LangLang/Application/DTO/EmailCredentials.cs
@@ -0,0 +1,17 @@
+﻿namespace LangLang.Application.Utility.Email;
+
+public class EmailCredentials
+{
+    public string EmailAddress { get; }
+    public string Password { get; }
+    public string Host { get; }
+    public int Port { get; }
+    
+    public EmailCredentials(string email, string password, string host, int port)
+    {
+        EmailAddress = email;
+        Password = password;
+        Host = host;
+        Port = port;
+    }
+}
