b4899e72a2540ef96ca99898fe8c5a2f6bd20843
Milan Lazarevic <mlazarevic662@gmail.com>
2024-05-20 16:40:57 +0200

    update director menu window

--- LangLang/WPF/ViewModels/Director/DirectorMenuViewModel.cs
+++ LangLang/WPF/ViewModels/Director/DirectorMenuViewModel.cs
@@ -2,6 +2,7 @@
 using LangLang.Application.UseCases.Authentication;
 using LangLang.Application.Utility.Navigation;
 using LangLang.WPF.MVVM;
+using LangLang.WPF.ViewModels.Common;
 using LangLang.WPF.ViewModels.Factories;
 using System;
 
@@ -9,22 +10,74 @@ namespace LangLang.WPF.ViewModels.Director
 {
     public class DirectorMenuViewModel : ViewModelBase, INavigableDataContext
     {
-        public RelayCommand OpenTutorTableCommand { get; set; }
+        public RelayCommand NavCommand { get; set; }
         public RelayCommand LogoutCommand { get; set; }
 
         private readonly ILoginService _loginService;
         private readonly INavigationService _navigationService;
         private readonly Domain.Model.Director _loggedInUser;
+        private readonly ILangLangViewModelFactory _viewModelFactory;
 
+        private ViewModelBase currentViewModel;
+
+        private TutorOverviewViewModel? tutorOverviewViewModel;
+        private CourseOverviewViewModel? courseOverviewViewModel;
+        private ExamOverviewViewModel? examOverviewViewModel;
+        private TutorOverviewViewModel TutorOverviewViewModel
+        {
+            get
+            {
+                if (tutorOverviewViewModel == null)
+                {
+                    tutorOverviewViewModel = (TutorOverviewViewModel)_viewModelFactory.CreateViewModel(ViewType.Tutor);
+                }
+
+                return tutorOverviewViewModel;
+            }
+        }
+        private ExamOverviewViewModel ExamOverviewViewModel
+        {
+            get
+            {
+                if (examOverviewViewModel == null)
+                {
+                    examOverviewViewModel = (ExamOverviewViewModel)_viewModelFactory.CreateViewModel(ViewType.Exam);
+                }
+
+                return examOverviewViewModel;
+            }
+        }
+        private CourseOverviewViewModel CourseOverviewViewModel
+        {
+            get
+            {
+                if (courseOverviewViewModel == null)
+                {
+                    courseOverviewViewModel = (CourseOverviewViewModel)_viewModelFactory.CreateViewModel(ViewType.Course);
+                }
+
+                return courseOverviewViewModel;
+            }
+        }
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => currentViewModel;
+            private set => SetField(ref currentViewModel, value);
+        }
 
         public NavigationStore NavigationStore { get; }
 
-        public DirectorMenuViewModel(ILoginService loginService, INavigationService navigationService, NavigationStore navigationStore, IAuthenticationStore authenticationStore)
+        public DirectorMenuViewModel(ILoginService loginService, INavigationService navigationService, NavigationStore navigationStore, 
+            IAuthenticationStore authenticationStore, ILangLangViewModelFactory viewModelFactory)
         {
             _loginService = loginService;
             _navigationService = navigationService;
             NavigationStore = navigationStore;
-            OpenTutorTableCommand = new RelayCommand(execute => OpenTutorTable());
+            NavCommand = new RelayCommand(execute => OnNav(execute as string));
+            _viewModelFactory = viewModelFactory;
+            CurrentViewModel = CourseOverviewViewModel;
+            //OpenTutorTableCommand = new RelayCommand(execute => OpenTutorTable());
             LogoutCommand = new RelayCommand(execute => Logout());
             _loggedInUser = (Domain.Model.Director?)authenticationStore.CurrentUser.Person ??
                                 throw new InvalidOperationException(
@@ -32,9 +85,15 @@ namespace LangLang.WPF.ViewModels.Director
         }
 
 
-        private void OpenTutorTable()
+        private void OnNav(string? destination)
         {
-            _navigationService.Navigate(ViewType.TutorTable);
+            CurrentViewModel = destination switch
+            {
+                "courses" => CourseOverviewViewModel,
+                "exams" => ExamOverviewViewModel,
+                "tutor" => TutorOverviewViewModel,
+                _ => CurrentViewModel
+            };
         }
 
 
