066173b77a711943c6809031b5bac9a8a974ee45
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-05-22 19:46:25 +0200

    Add paginator

---
+++ LangLang/Repositories/Json/Util/PagingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LangLang.Repositories.Json.Util;
+
+public static class PagingExtensions
+{
+    public static List<T> GetPage<T>(this List<T> original, int pageNumber, int recordsPerPage)
+    {
+        if (pageNumber <= 0 || recordsPerPage <= 0)
+            throw new ArgumentException("Arguments must be positive integers.");
+        if (pageNumber * recordsPerPage + recordsPerPage >= original.Count)
+            return new List<T>();
+        return original.GetRange((pageNumber - 1) * recordsPerPage, recordsPerPage);
+    } 
+}
