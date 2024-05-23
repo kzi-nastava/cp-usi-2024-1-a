d0a1abe772b1f1c70c8845da7f0c378592afa3f0
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-05-23 10:14:19 +0200

    Add pagination for exams

--- LangLang/Domain/RepositoryInterfaces/IExamRepository.cs
+++ LangLang/Domain/RepositoryInterfaces/IExamRepository.cs
@@ -9,4 +9,6 @@ public interface IExamRepository : IRepository<Exam>
     public List<Exam> GetByDate(DateOnly date);
     public List<Exam> GetForTimePeriod(DateTime from, DateTime to);
     public List<Exam> GetByTutorId(string tutorId);
+    public List<Exam> GetAllForPage(int pageNumber, int examsPerPage);
+    public List<Exam> GetByTutorIdForPage(string tutorId, int pageNumber, int examsPerPage);
 }
