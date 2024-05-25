363afa84564797301bf484441a02cbd95f14f3e4
Milan Lazarevic <mlazarevic662@gmail.com>
2024-05-25 12:32:37 +0200

    Add ExamTutorSelectionDto and AutoExamTutorSelector

---
+++ LangLang/Application/DTO/ExamTutorSelectionDto.cs
@@ -0,0 +1,24 @@
+﻿using LangLang.Domain.Model;
+using System;
+using System.Collections.Generic;
+
+namespace LangLang.Application.DTO;
+
+public class ExamTutorSelectionDto
+{
+    public Language Language { get; }
+    public LanguageLevel LanguageLevel { get; }
+    public DateOnly? Date { get; }
+    public TimeOnly? Time { get; }
+    public Exam? ExceptionExam { get; }
+
+    public ExamTutorSelectionDto(Language language, LanguageLevel languageLevel, DateOnly date, TimeOnly time, Exam? exceptionExam = null)
+    {
+        Language = language;
+        LanguageLevel = languageLevel;
+        Date = date;
+        Time = time;
+        ExceptionExam = exceptionExam;
+    }
+
+}
---
+++ LangLang/Application/UseCases/TutorSelection/AutoExamTutorSelector.cs
@@ -0,0 +1,42 @@
+﻿using LangLang.Application.DTO;
+using LangLang.Application.UseCases.User;
+using LangLang.Application.Utility.Timetable;
+using LangLang.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LangLang.Application.UseCases.TutorSelection;
+
+public class AutoExamTutorSelector : IAutoExamTutorSelector
+{
+    private readonly ITutorService _tutorService;
+    private readonly ITimetableService _timetableService;
+
+    public AutoExamTutorSelector(ITutorService tutorService, ITimetableService timetableService)
+    {
+        _tutorService = tutorService;
+        _timetableService = timetableService;
+    }
+
+    public Tutor? Select(ExamTutorSelectionDto dto)
+    {
+        var tutors = FilterEligibleTutors(_tutorService.GetAllTutors(), dto);
+        return tutors.MaxBy(tutor => tutor.GetScore(dto.Language, dto.LanguageLevel));
+    }
+
+    private List<Tutor> FilterEligibleTutors(List<Tutor> tutors, ExamTutorSelectionDto dto)
+    {
+        return tutors.Where(tutor =>
+            tutor.KnowsLanguage(dto.Language, dto.LanguageLevel) &&
+            dto.Time != null && dto.Date != null &&
+            IsAvaliableForExam(tutor, dto.Date.Value, dto.Time.Value, dto.ExceptionExam)
+            ).ToList();
+    }
+
+    private bool IsAvaliableForExam(Tutor tutor, DateOnly date,TimeOnly time,  Domain.Model.Exam? exam)
+    {
+        List<TimeOnly> avaliableTimes = _timetableService.GetAvailableExamTimes(date, tutor, null, exam);
+        return avaliableTimes.Contains(time);
+    }
+}
