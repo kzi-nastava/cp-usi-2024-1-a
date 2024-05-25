cd41432d8620373492ba4b5b7b2e0b6f82afe9d7
Filip Tot <filipsombor@gmail.com>
2024-05-25 19:50:20 +0200

    Update view model to use event system instead

--- LangLang/WPF/ViewModels/Director/TutorOverviewViewModel.cs
+++ LangLang/WPF/ViewModels/Director/TutorOverviewViewModel.cs
@@ -21,7 +21,6 @@ namespace LangLang.WPF.ViewModels.Director
     {
         private readonly ITutorService _tutorService;
         private readonly ILanguageService _languageService;
-        private readonly IRegisterService _registerService;
         private readonly IAccountService _accountService;
         private readonly IUserValidator _userValidator;
         private readonly INavigationService _navigationService;
@@ -42,6 +41,10 @@ namespace LangLang.WPF.ViewModels.Director
         public bool changedLanguages;
         public bool changedEmailOrPassword;
         public bool selectingTutor;
+
+        public event Action? RemoveKnownLanguages;
+        public event KnownLanguageAddedHandler? KnownLanguageAdded;
+        public delegate void KnownLanguageAddedHandler(int languageIndex, int levelIndex, ObservableCollection<string?> languages, ObservableCollection<LanguageLevel> levels);
 
         private string name = "";
         public string Name
@@ -184,26 +187,13 @@ namespace LangLang.WPF.ViewModels.Director
                 OnPropertyChanged();
             }
         }
-        private TutorOverviewView? _window;
-        public TutorOverviewView Window
-        {
-            get => _window!;
-            set
-            {
-                if(_window == null)
-                {
-                    _window = value;
-                }
-            }
-        }
 
         public NavigationStore NavigationStore { get; }
 
-        public TutorOverviewViewModel(NavigationStore navigationStore, INavigationService navigationService, IRegisterService registerService, IAccountService accountService, ITutorService tutorService, ILanguageService languageService, IUserValidator userValidator, IUserProfileMapper userProfileMapper)
+        public TutorOverviewViewModel(NavigationStore navigationStore, INavigationService navigationService, IAccountService accountService, ITutorService tutorService, ILanguageService languageService, IUserValidator userValidator, IUserProfileMapper userProfileMapper)
         {
             NavigationStore = navigationStore;
             _navigationService = navigationService;
-            _registerService = registerService;
             _accountService = accountService;
             _tutorService = tutorService;
             _languageService = languageService;
@@ -262,7 +252,7 @@ namespace LangLang.WPF.ViewModels.Director
                 languageIndex = Languages.IndexOf(value.Item1);
                 levelIndex = Levels.IndexOf(value.Item2);
             }
-            Window.AddKnownLanguage(languageIndex, levelIndex, Languages, Levels);
+            KnownLanguageAdded?.Invoke(languageIndex, levelIndex, Languages, Levels);
             if (value == null)
             {
                 changedLanguages = true;
@@ -303,7 +293,7 @@ namespace LangLang.WPF.ViewModels.Director
 
         private void SwitchKnownLanguages(Domain.Model.Tutor selectedItem)
         {
-            Window.RemoveKnownLanguages();
+            RemoveKnownLanguages?.Invoke();
             KnownLanguages.Clear();
             foreach (var tuple in selectedItem.KnownLanguages)
                 AddKnownLanguage(new Tuple<string, LanguageLevel>(tuple.Item1.Name, tuple.Item2));
@@ -427,7 +417,7 @@ namespace LangLang.WPF.ViewModels.Director
             selectedItem = null;
 
             selectingTutor = true;
-            Window.RemoveKnownLanguages(); 
+            RemoveKnownLanguages?.Invoke(); 
             KnownLanguages.Clear();
             selectingTutor = false;
         }
