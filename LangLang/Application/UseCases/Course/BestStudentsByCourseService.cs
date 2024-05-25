5638e3a8481af794fb3b176512324855773e9427
marijaparezanin <marija.parezanin@mensa.ba>
2024-05-25 22:53:59 +0200

    Add string format for email body

--- LangLang/Application/UseCases/Course/BestStudentsByCourseService.cs
+++ LangLang/Application/UseCases/Course/BestStudentsByCourseService.cs
@@ -73,23 +73,7 @@ public class BestStudentsByCourseService: IBestStudentsByCourseService
                 string studentName = student.Name + " " + student.Surname;
                 string email = _accountService.GetEmailByUserId(studentId);
                 string emailSubject = "Congratulations on Your Outstanding Performance!";
-                string emailBody = $@"
-        Dear {studentName},
-
-        I hope this email finds you well.
-
-        I am writing to extend my heartfelt congratulations to you for your exceptional performance in our {courseName}. Your dedication, knowledge, and active participation have truly set you apart from your peers. It has been a pleasure to witness your growth and commitment throughout the course.
-
-        Your impressive achievements are a testament to your hard work and intellectual curiosity. You have consistently demonstrated a deep understanding of the material and have actively contributed to class discussions, enriching the learning experience for everyone.
-
-        As one of the top students in this course, you have exemplified the qualities of an outstanding scholar. I have no doubt that you will continue to excel in your academic and professional endeavors.
-
-        Thank you for your dedication and enthusiasm. It has been an honor to have you in the class, and I look forward to seeing all the great things you will accomplish in the future.
-
-        Warm regards,
-
-        LangLang school
-    ";
+                string emailBody = string.Format(Utility.BestStudentsConstants.bestStudentsAppreciationEmailBody, studentName, courseName);
                 _emailService.SendEmail(email, emailSubject, emailBody);
             }
 
