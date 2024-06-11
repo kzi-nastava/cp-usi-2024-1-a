35ab5909d42632961c49bfaca0221e4604372314
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-06-11 11:51:53 +0200

    Add CLI with LoginMenu

--- LangLang/App.xaml.cs
+++ LangLang/App.xaml.cs
@@ -1,15 +1,26 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using LangLang.CLI.Views;
 using LangLang.HostBuilders;
 using LangLang.WPF.Views.Common;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace LangLang
 {
     public partial class App : System.Windows.Application
     {
-        private readonly IHost _host = CreateHostBuilder().Build();
+        private readonly IHost _host;
+
+        public App()
+        {
+            var args = Environment.GetCommandLineArgs();
+            _host = args.Contains("--cli") ? CreateCliHostBuilder().Build() : CreateHostBuilder().Build();
+        }
 
         private static IHostBuilder CreateHostBuilder(string[]? args = null)
         {
@@ -22,12 +33,43 @@ namespace LangLang
                 .AddWindows();
         }
 
+        private static IHostBuilder CreateCliHostBuilder(string[]? args = null)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureAppConfiguration((_, builder) => builder.AddUserSecrets<App>())
+                .AddRepositories()
+                .AddServices()
+                .AddStores()
+                .AddMenus();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             _host.Start();
-
-            Window window = _host.Services.GetRequiredService<LoginWindow>();
-            window.Show();
+            
+            if (e.Args.Contains("--cli"))
+            {
+                try
+                {
+                    const int attachParentProcess = -1;
+                    if (AttachConsole(attachParentProcess))
+                    {
+                        ICliMenu loginMenu = _host.Services.GetRequiredService<LoginMenu>();
+                        loginMenu.Show();
+                    }
+                }
+                finally
+                {
+                    FreeConsole();
+                    Shutdown();
+                }
+            }
+            else
+            {
+                Window window = _host.Services.GetRequiredService<LoginWindow>();
+                window.Show();
+            }
             
             base.OnStartup(e);
         }
@@ -37,5 +79,11 @@ namespace LangLang
             _host.StopAsync();
             base.OnExit(e);
         }
+        
+        [DllImport("kernel32")]
+        private static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("kernel32")]
+        private static extern bool FreeConsole();
     }
 }
